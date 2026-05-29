32242f33fc4bf8b8ec4da08e4cc7c37e65e3fc9c
BOTTINI Thomas <thomas.bottini@cnrs.fr>
2026-05-29 10:07:55 +0200

    🌴

--- 20260604-refar-institutions/make.sh
+++ 20260604-refar-institutions/make.sh
@@ -1,8 +1,9 @@
 rm refar-institutions.svg
+rm refar-institutions.png
 
 d2 --layout tala \
    refar-institutions.d2 refar-institutions.svg
 
 sed -i '' 's/UNLICENSED COPY//g' refar-institutions.svg
 
-open refar-institutions.svg
+rsvg-convert refar-institutions.svg > refar-institutions.png
